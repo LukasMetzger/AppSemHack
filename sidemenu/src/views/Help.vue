f1e5603066547a8bd7b2f2aa6dea1eef72684477
cdogan <d_hawk@mail.de>
2020-11-13 17:50:06 +0100

    fix not needed icons.

--- sidemenu/src/views/Help.vue
+++ sidemenu/src/views/Help.vue
@@ -27,31 +27,27 @@
                 <img style="width: 15px; height: 15px;" :src="i.funnel">
               </ion-chip>
             </div>
-            <div class="flexRow">
+            <div style="margin-top: 20px;" class="flexRow">
               <a>
                 <ion-chip outline color="dark">
-                  <ion-icon name="videocam"></ion-icon>
                   <ion-label>FAQ</ion-label>
                   <ion-icon name="close-circle"></ion-icon>
                 </ion-chip>
               </a>
               <a>
                 <ion-chip outline color="dark">
-                  <ion-icon name="videocam"></ion-icon>
                   <ion-label>BLOG</ion-label>
                   <ion-icon name="close-circle"></ion-icon>
                 </ion-chip>
               </a>
               <a>
                 <ion-chip outline color="dark">
-                  <ion-icon name="videocam"></ion-icon>
                   <ion-label>Channels</ion-label>
                   <ion-icon name="close-circle"></ion-icon>
                 </ion-chip>
               </a>
               <a>
                 <ion-chip outline color="dark">
-                  <ion-icon name="videocam"></ion-icon>
                   <ion-label>Events</ion-label>
                   <ion-icon name="close-circle"></ion-icon>
                 </ion-chip>
